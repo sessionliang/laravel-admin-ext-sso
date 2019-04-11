1da1a47f6ae73298df80a374abc81c3f2fc598e7
sessionliang <sessionliang@outlook.com>
2019-04-11 17:41:52 +0800

    -

--- config/sso.php
+++ config/sso.php
@@ -5,10 +5,10 @@ return [
     /**
      * app_id
      */
-    'app_id' => config('SSO_APP_ID', ''),
+    'app_id' => env('SSO_APP_ID', ''),
 
     /**
      * app_secret
      */
-    'app_secret' => config('SSO_APP_SECRET', ''),
+    'app_secret' => env('SSO_APP_SECRET', ''),
 ];
